dc4d09634c903f7bc4588a0366f1899e2e8404a0
Calil-Silva <ka.renner@hotmail.com>
2022-01-19 15:46:34 -0300

    Rollback ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,18 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-module.exports = {
+export default {
     type: 'postgres',
     url: process.env.DATABASE_URL,
     migrationsTableName: 'migrations',
-    entities: ['dist/**/*.entity.js'],
+    entities: ['dist/entities/*.js'],
     migrations: ['dist/migrations/*.js'],
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
     cli: {
         migrationsDir: 'src/migrations',
         entitiesDir: 'dist/entities/*.js',
