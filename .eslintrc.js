8aaa714cc58efd72d2c75f5baeb1246032012313
Calil-Silva <ka.renner@hotmail.com>
2022-01-19 12:32:33 -0300

    Template front

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'prefer-destructuring': 0,
+        'import/prefer-default-export': 0,
+        'no-useless-escape': 0,
+        'comma-dangle': 0,
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'comma-spacing': ['error', { before: false, after: true }],
+        'semi-spacing': ['error', { before: false, after: true }],
+        'object-curly-spacing': ['error', 'always'],
+        'key-spacing': ['error', { beforeColon: false }],
+        'padded-blocks': ['error', 'never'],
+        'no-multiple-empty-lines': ['error', { max: 1 }],
+        'lines-between-class-members': [
+            'error',
+            'always',
+            { exceptAfterSingleLine: true },
+        ],
+        'space-before-function-paren': 'off',
+        'space-before-blocks': ['error', 'always'],
+        'eol-last': ['error', 'always'],
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                selector: 'interface',
+                format: ['PascalCase'],
+                custom: {
+                    regex: '^I[A-Z]',
+                    match: false,
+                },
+            },
+        ],
+        '@typescript-eslint/type-annotation-spacing': [
+            'error',
+            { after: true, before: false },
+        ],
+        'arrow-spacing': ['error', { before: true, after: true }],
+        'no-console': 'warn',
+    },
+};
