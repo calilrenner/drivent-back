dd17a3509293391bea500e3de247ea5dae7de8ba
Calil-Silva <ka.renner@hotmail.com>
2022-01-28 19:12:25 -0300

    Preparing for deploy

--- src/routers/client/event.ts
+++ src/routers/client/event.ts
@@ -5,7 +5,7 @@ import * as controller from '@/controllers/client/event';
 const router = Router();
 
 router.get('/', controller.get);
-router.post('/schedule', controller.postUserEvent);
+// router.post('/schedule', controller.postUserEvent);
 router.get('/:id', controller.getEventsByDayId);
 
 export default router;
